fe043996297d77b7b29e7f2c41c7ddd99bc0224b
umccenter <umc.center01@gmail.com>
2025-01-26 01:50:25 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1737874121,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"muhammadi quranic font.ttf",
+		"muhammadi_quranic_font-webfont.woff",
+		"muhammadi_quranic_font-webfont.woff2",
+		"stylesheet1.css",
+		"firebase.js"
+	]
+}
